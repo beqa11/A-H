0a5dca04f58f8cd99f25f55f21475eaae02f4807
beqa11 <104617961+beqa11@users.noreply.github.com>
2023-01-15 13:49:41 +0400

    Add files via upload

---
+++ HomePageGeo.js
@@ -0,0 +1,20 @@
+var img = document.getElementById('img');
+
+var slides=['/fotoebi/1.png','/fotoebi/2.png'
+,'/fotoebi/3.png','/fotoebi/4.png'
+,'/fotoebi/5.png','/fotoebi/6.png'];
+
+var Start=0;
+
+function slider(){
+    if(Start<slides.length){
+        Start=Start+1 ;
+    }
+    else{
+        Start=1;
+    }
+    console.log(img);
+    img.innerHTML = "<img src="+slides[Start-1]+">";
+   
+}
+setInterval(slider,5000);
